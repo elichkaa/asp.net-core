ecd76ad79d09585c583f5d7907013cc92e54161a
elichka <ed44362072@edu.mon.bg>
2021-01-29 11:29:39 +0200

    Create Template Project

---
+++ aspnet template/Data/Template.Data/EntityIndexesConfiguration.cs
@@ -0,0 +1,23 @@
+﻿namespace Template.Data
+{
+    using System.Linq;
+
+    using Template.Data.Common.Models;
+
+    using Microsoft.EntityFrameworkCore;
+
+    internal static class EntityIndexesConfiguration
+    {
+        public static void Configure(ModelBuilder modelBuilder)
+        {
+            // IDeletableEntity.IsDeleted index
+            var deletableEntityTypes = modelBuilder.Model
+                .GetEntityTypes()
+                .Where(et => et.ClrType != null && typeof(IDeletableEntity).IsAssignableFrom(et.ClrType));
+            foreach (var deletableEntityType in deletableEntityTypes)
+            {
+                modelBuilder.Entity(deletableEntityType.ClrType).HasIndex(nameof(IDeletableEntity.IsDeleted));
+            }
+        }
+    }
+}
---
+++ aspnet template/Web/Template.Web/Areas/Administration/Controllers/AdministrationController.cs
@@ -0,0 +1,14 @@
+﻿namespace Template.Web.Areas.Administration.Controllers
+{
+    using Template.Common;
+    using Template.Web.Controllers;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    [Area("Administration")]
+    public class AdministrationController : BaseController
+    {
+    }
+}
